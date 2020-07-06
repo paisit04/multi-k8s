a7469e8750228be4dfea773eb810cfab9c60b8a1
Paisit Jarunnamsiri <paisitj@Paisits-MacBook-Pro.local>
2020-07-06 17:42:07 +0700

    added deployment script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t paisit04/multi-client:latest -t paisit04/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t paisit04/multi-server:latest -t paisit04/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t paisit04/multi-worker:latest -t paisit04/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push paisit04/multi-client:latest
+docker push paisit04/multi-client:$SHA
+docker push paisit04/multi-server:latest
+docker push paisit04/multi-server:$SHA
+docker push paisit04/multi-worker:latest
+docker push paisit04/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployment/server-deployment client=paisit04/multi-server:$SHA
+kubectl set image deployment/client-deployment client=paisit04/multi-client:$SHA
+kubectl set image deployment/worker-deployment client=paisit04/multi-worker:$SHA
